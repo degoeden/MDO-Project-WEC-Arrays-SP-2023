5e9deb2c17ee0e81fba9e40ce0923644968525e7
Pat Thornton <pmt58@cornell.edu>
2023-03-02 12:16:06 -0500

    Basic update to Econ module

--- Econ.py
+++ Econ.py
@@ -1,7 +1,20 @@
 # Paceholder data
+import numpy as np
+#import pandas as pd
 
+
+x0=np.array([0,1,2,3,4,5])# power in [units]
+f0=np.array([1,2,3,4,5,6]) # Frequency in [units]
 # Incoming internal variables:
 # Power vs frequency ouput from WEC array elements
+def func_init(): # __main__ goes somewhere... revisit.
+    return print('This is the main function')
+
+def sub_func_acdc1(p,):#AC to DC conversion at power bank
+    return print('test')
+
+def sub_func_dcac1(p,):#DC to AC conversion step up to transmission voltage
+    return print('test')
 
 # Design Parameters incoming:
 # Cost of capital, lifetime costs, assumptions based on energy demand and rates
