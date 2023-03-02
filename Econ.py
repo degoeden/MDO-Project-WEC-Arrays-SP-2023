3c73f267f04868076315bfca6c4199164d93cafa
Pat Thornton <pmt58@cornell.edu>
2023-03-02 12:35:31 -0500

    expand on submodules

--- Econ.py
+++ Econ.py
@@ -10,12 +10,20 @@ f0=np.array([1,2,3,4,5,6]) # Frequency in [units]
 def func_init(): # __main__ goes somewhere... revisit.
     return print('This is the main function')
 
-def sub_func_acdc1(p,):#AC to DC conversion at power bank
+def sub_func_acdc1(p,cap):#AC to DC conversion at power bank
+    #integrate power of voltage produced at WEC and distribute to power bank, return max power available for distribution
     return print('test')
 
-def sub_func_dcac1(p,):#DC to AC conversion step up to transmission voltage
-    return print('test')
+def sub_func_dcac1(p_in,r):#DC to AC conversion step up to transmission voltage
+    #implement losses during power conversion.. need relevent parameters
+    p_out=np.mean(p_in*r)# some calculation of losses
+    eff=np.mean(p_out/p_in) #some calculation of efficiency, with weighting or using net energy 
+    return p_out,eff
 
+def cost_module(p_out):
+    mean_rate=1# placeholder for variable rates.
+    revenue=p_out*mean_rate
+    return revenue
 # Design Parameters incoming:
 # Cost of capital, lifetime costs, assumptions based on energy demand and rates
 
