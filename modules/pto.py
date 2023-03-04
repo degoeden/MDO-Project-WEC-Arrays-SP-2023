0a2b690445b2fdba0522819f644ac3cb9d2ee142
degoeden <degoeden8@gmail.com>
2023-03-04 10:58:27 -0500

    power out will be somewhere else

--- modules/pto.py
+++ modules/pto.py
@@ -1,13 +1,12 @@
 # PTO module: Handles power take-off and controller
 # Inputs: damping [Ns/m], stiffness [N/m], complex_body_motion [m]
-# Outputs: pto_force [N], power_out [W]
+# Outputs: pto_force [N]
 
 from scipy import signal
 
 def pto(d,k,XI):
     s = signal.TransferFunction('s')
     pto_force = (s*d + k)*XI
-    power_out = 0
-    return pto_force,power_out
+    return pto_force
 
 # help!!!
