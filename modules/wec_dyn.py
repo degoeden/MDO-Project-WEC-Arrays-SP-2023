a4624db2cdf10b0d3b52f31519868f6e443f357e
degoeden <degoeden8@gmail.com>
2023-03-06 17:23:51 -0500

    new comments on wec dyn

--- modules/wec_dyn.py
+++ modules/wec_dyn.py
@@ -1,3 +1,6 @@
+# Calculates the body motion RAO based on the wave exciting force RAO
+# Inputs: frequency, wave exciting force RAO, Added Mass, Added Damping, Hydrostatic Restoring, mass, pto damping, pto stiffness
+# Outputs: heave motion RAO
 
 def wecdyn(omega,F,A,B,C,m,d,k):
     XI = F/(-(A+m)*omega**2 + (B+d)*omega*1j + C+k)
