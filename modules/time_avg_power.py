154fd58fdfbebcda87a93668b8afedf4369cd983
degoeden <degoeden8@gmail.com>
2023-03-07 13:31:10 -0500

    something little

--- modules/time_avg_power.py
+++ modules/time_avg_power.py
@@ -1,7 +1,7 @@
 # Calculates Time Average Power based on RAO of heave motion
-# Inputs: heave motion RAO, pto damping, and wave amplitude
+# Inputs: heave motion RAO, pto damping, frequency, and wave amplitude
 # Outputs: Time Average Power
 
-def time_avg_power(XI,d,A):
-    power = (1/2)*d*abs(A*XI)**2
+def time_avg_power(XI,d,omega,A):
+    power = (1/2)*d*abs(A*XI*omega*1j)**2
     return power
