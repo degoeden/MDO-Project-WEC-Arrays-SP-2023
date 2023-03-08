6db7601de1c2bd21066ea2651fe5b546eeb9f945
degoeden <degoeden8@gmail.com>
2023-03-08 18:04:15 -0500

    git

--- modules/wec_dyn.py
+++ modules/wec_dyn.py
@@ -2,6 +2,6 @@
 # Inputs: frequency, wave exciting force RAO, Added Mass, Added Damping, Hydrostatic Restoring, mass, pto damping, pto stiffness
 # Outputs: heave motion RAO
 
-def wecdyn(omega,F,A,B,C,m,d,k):
+def wec_dyn(omega,F,A,B,C,m,d,k):
     XI = F/(-(A+m)*omega**2 + (B+d)*omega*1j + C+k)
     return XI
