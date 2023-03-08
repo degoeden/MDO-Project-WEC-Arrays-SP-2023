5d2e36f91d55dd01b5c23d4282e739dfa5393485
oliviavitale35 <119702956+oliviavitale35@users.noreply.github.com>
2023-03-08 18:03:51 -0500

    Update hydrodyno.py
    added added mass and damping coeff and exciting force calcs as outputs

--- capy/hydrodyno.py
+++ capy/hydrodyno.py
@@ -20,5 +20,14 @@ def hydrodynamics(body,w,B):
         for dof in body.dofs]
     results = [solver.solve(pb,keep_details=(True)) for pb in sorted(problems)]
     dataset = cpt.assemble_dataset(results)
-    rao = cpt.post_pro.rao(dataset)
-    return rao
+    rao = cpt.post_pro.rao(dataset)                                 # response amplitude operator
+    a = dataset['added_mass'].sel(radiating_dof='Heave',
+                                     influenced_dof='Heave')        # added mass in heave
+    b = dataset['radiation_damping'].sel(radiating_dof='Heave',
+                                     influenced_dof='Heave')        # damping coeff in heave
+    test = cpt.DiffractionProblem(body=body1, omega=1, wave_direction=0.)
+    res = solver.solve(test)
+    FK = froude_krylov_force(test)['Heave']
+    dif = res.forces['Heave']
+    ex_force = FK + dif                                             # exciting force in heave
+    return rao,a,b,ex_force
