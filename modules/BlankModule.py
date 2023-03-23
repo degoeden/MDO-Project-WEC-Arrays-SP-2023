2cd7d470f7230b4e76c58df175af79b483f03880
degoeden <degoeden8@gmail.com>
2023-03-23 12:51:34 -0400

    more organization

--- modules/BlankModule.py
+++
@@ -1,7 +0,0 @@
-# BLANK module - Handles Nothing
-# Inputs: not_much [-], even_less [-]
-# Outputs: nothing_important [-]
-
-def BLANK_function(not_much, even_less):
-  nothing_important = not_much + even_less
-  return nothing_important
