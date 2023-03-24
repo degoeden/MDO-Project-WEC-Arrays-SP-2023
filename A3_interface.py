5d295039bcb736224c4a231c4e7189a01ce8b2d1
degoeden <degoeden8@gmail.com>
2023-03-24 14:52:10 -0400

    Made out of bounds check nicer

--- A3_interface.py
+++ A3_interface.py
@@ -2,27 +2,33 @@
 import scipy.optimize as scipy_opt
 import modules.model_2WECs as model
 
+def outofbounds(x,p):
+    arewe = False
+    if x[0] < 2.5:
+        are_we = True
+    if x[0] > 15:
+        are_we = True
+    if x[1] < 2*x[0]:
+        are_we = True
+    if x[1] > 5*x[0]:
+        are_we = True
+    for i in range(p[3]):
+        if x[2+i*2] < 0:
+            are_we = True
+        if x[3+i*2] < 0:
+            are_we = True
+        if x[2+i*2] > 500:  # may need a more concrete number
+            are_we = True
+        if x[3+i*2] > 500:  # may need a more concrete number
+            are_we = True
+    return are_we
+
 def gradient_objective(x,p):
     Power_out,efficiency,LCOE = model.run(x,p)
     if LCOE > 10:
         Power_out = 0
-    if x[0] < 2.5:
+    if outofbounds(x,p):
         Power_out = 0
-    if x[0] > 15:
-        Power_out = 0
-    if x[1] < 2*x[0]:
-        Power_out = 0
-    if x[1] > 5*x[0]:
-        Power_out = 0
-    for i in range(p[3]):
-        if x[2+i*2] < 0:
-            Power_out = 0
-        if x[3+i*2] < 0:
-            Power_out = 0
-        if x[2+i*2] > 500:  # may need a more concrete number
-            Power_out = 0
-        if x[3+i*2] > 500:  # may need a more concrete number
-            Power_out = 0
     
     return -1*Power_out
 
