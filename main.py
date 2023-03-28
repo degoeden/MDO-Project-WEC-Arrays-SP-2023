9d7a3c71bdb4a2e245798d75c6377112d5cf2884
degoeden <degoeden8@gmail.com>
2023-03-28 11:07:56 -0400

    GA

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# To run A3b code, import the A3 interface and run the desired A3 function: gradient_method or heuristic method
+# To run A3b code, import the A3 interface and run the desired A3 function: gradient_method or heuristic_method
 import A3_interface as A3
 
 # Initial WEC design
 r = 8
-L = 20
+space = 3
 d1 = 500
 d2 = 500
 k1 = 500
 k2 = 500
-x0 = [r,L,d1,k1,d2,k2]
+x0 = [r,space,d1,k1,d2,k2]
 
 # Parameters
 omega = 1.047
