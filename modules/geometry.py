aac4bfb5b8d804fc46286b5e0d465f01e8d5a2ef
degoeden <degoeden8@gmail.com>
2023-04-03 16:48:36 -0400

    recreated because I was scared

--- modules/geometry.py
+++ modules/geometry.py
@@ -1,18 +1,29 @@
 import capytaine as cpt
 
 def run(r,wecx,wecy):
-    def generate_body(xyz,r):
-        mesh1 = cpt.meshes.predefined.mesh_sphere(radius=r,center=(xyz[0],xyz[1],xyz[2]))
+    #def generate_body(xyz,r):
+    #    mesh1 = cpt.meshes.predefined.mesh_sphere(radius=r,center=(xyz[0],xyz[1],xyz[2]))
+    #    body = cpt.FloatingBody(mesh1)
+    #    body.add_translation_dof(name='Heave')
+    #    body = body.immersed_part()
+    #    body.name = f'{xyz[0]}_{xyz[1]}_{xyz[2]}'
+    #return body
+    
+    #xyzees = []
+    #for i in range(len(wecx)):
+    #    xyzees.append(wecx[i],wecy[i],0)
+    
+    #bodies = [generate_body(xyz) for xyz in xyzees ]
+    
+    def get_body(r,x,y):
+        mesh1 = cpt.meshes.predefined.mesh_sphere(radius=r,center=(x,y,0))
         body = cpt.FloatingBody(mesh1)
         body.add_translation_dof(name='Heave')
         body = body.immersed_part()
-        body.name = f'{xyz[0]}_{xyz[1]}_{xyz[2]}'
-    return body
-    
-    xyzees = []
-    for i in range(len(wecx)):
-        xyzees.append(wecx[i],wecy[i],0)
-    
-    bodies = [generate_body(xyz) for xyz in xyzees ]
+        body.name = f'{x}_{y}_{0}'
+
+    bodies = []    
+    for i in range(wecx):
+        bodies.append(get_body(r,wecx[i],wecy[i]))
 
     return bodies
--- modules/model_4WECs.py
+++ modules/model_4WECs.py
@@ -1,5 +1,6 @@
 import modules.Econ as Econ
 import modules.geometry as geom
+import modules.hydro as hydro
 from modules.wec_dyn import wec_dyn as wec_dyn
 from modules.time_avg_power import time_avg_power as time_avg_power
 from numpy import pi as pi
@@ -32,8 +33,9 @@ def run(x,p):
     # Mass Calculation
     m = rho_wec*4*pi/3*wec_radius**3
 
-    # Hydro Module
-    hydro_results = geom.run(wec_radius,wecx,wecy)
+    # Geometry and Hydro Modules
+    bodies = geom.run(wec_radius,wecx,wecy)
+    hydro_results = hydro.run(bodies)
         # hydro_results = [Exciting Force RAO, Added mass, Wave damping, Hydrostatic restoring] for each WEC
 
     # Dynamics and Controls Modules
