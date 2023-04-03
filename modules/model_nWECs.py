4ffc4e6c6167957904b66e788831a3aa39d473ea
degoeden <degoeden8@gmail.com>
2023-04-03 16:08:04 -0400

    where are the model wec pies

---
+++ modules/model_nWECs.py
@@ -0,0 +1,8 @@
+import modules.Econ as Econ
+import modules.hydro as hydro
+from modules.wec_dyn import wec_dyn as wec_dyn
+from modules.time_avg_power import time_avg_power as time_avg_power
+from numpy import pi as pi
+import numpy as np
+
+#
