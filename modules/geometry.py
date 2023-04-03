775d642a20bec9884b6a9acb52de60ece67bc99f
oliviavitale35 <119702956+oliviavitale35@users.noreply.github.com>
2023-04-03 16:37:09 -0400

    Update geometry.py
    generate bods

--- modules/geometry.py
+++ modules/geometry.py
@@ -1,9 +1,18 @@
 import capytaine as cpt
 
 def run(r,wecx,wecy):
-
+    def generate_body(xyz,r):
+        mesh1 = cpt.meshes.predefined.mesh_sphere(radius=r,center=(xyz[0],xyz[1],xyz[2]))
+        body = cpt.FloatingBody(mesh1)
+        body.add_translation_dof(name='Heave')
+        body = body.immersed_part()
+        body.name = f'{xyz[0]}_{xyz[1]}_{xyz[2]}'
+    return body
+    
+    xyzees = []
     for i in range(len(wecx)):
-        # do make body x = wecx[i], y = wecy[i], bodies[i] = fcn(whatever...)
-
+        xyzees.append(wecx[i],wecy[i],0)
+    
+    bodies = [generate_body(xyz) for xyz in xyzees ]
 
     return bodies
