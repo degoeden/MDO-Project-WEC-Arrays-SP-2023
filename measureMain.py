45595f6242f74fedc208b2fd35c53fc68ec5ee21
cornell <kkhanal16@winona.edu>
2023-04-04 20:40:14 -0400

    measuring for genetic

--- measureMain.py
+++ measureMain.py
@@ -12,7 +12,7 @@ space = 3                           #   WEC spacing multiplier (multiplies by th
 d1 = 1e5                            #   PTO damping
 d2 = 1e5    
 x0 = [r,space,d1,d2]
-bnds=[[2.5,15],[4,20],[10,10**7],[10,10**7]]    #   Set bounds for design variables
+bnds=[[2.5,15],[2,20],[10,10**7],[10,10**7]]    #   Set bounds for design variables
 
 # Parameters
 omega = 1.047                       #   Wave Frequency
@@ -23,12 +23,22 @@ p = [omega,A,rho_wec,n_wec]
 opt={'xatol': 1e-3, 'disp': True} 
 
 
+popSize = [15,100]
+mutation = [(0.5,1),(0.4,1)]
+recombination = [0.8,0.9]
+
+measured = {'x':1}
+
+for pops,muts,recombs in zip(popSize,mutation,recombination):
+	opts = {'popsize':pops,'mutation':muts,'recombination':recombs}
+	starttime = time.time()
+	best = A3.heuristic_method(p,bnds,opts) 
+	end = time.time()
+	measured['time'] = end-starttime
+	measured['recom'] = (pops,muts,recombs)
+	measured['LCOE'] = best
 
 opts = {'popsize':15,'mutation':(0.5,1),'recombination':0.8}
-best = A3.heuristic_method(p,bnds,opts)      #   Heuristic Optimization
-
-
-
 #popsize=15, tol=0.01, mutation=(0.5, 1), recombination=0.7
 # ================================================================================= #
 # Run Optimization
@@ -36,5 +46,5 @@ best = A3.heuristic_method(p,bnds,opts)      #   Heuristic Optimization
 #best = A3.gradient_method(x0,p,bnds,opt)    #   Gradient Optimization
 # ================================================================================= #
 # Print Best
-print(best)
-
+print("THE TABLES HERE IT IS")
+print(measured)
