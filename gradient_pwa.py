93c9e85409777d5b5c46ca3862ae9a5d9aca4c29
degoeden <degoeden8@gmail.com>
2023-04-19 09:22:29 -0400

    new names

---
+++ gradient_pwa.py
@@ -0,0 +1,37 @@
+# To run A3b code, import the A3 interface and run the desired A3 function: gradient_method or heuristic_method
+import optimization_interfaces.A3_interface as A3
+import numpy as np
+# ================================================================================= #
+#                                   Set-Up                                          #
+# ================================================================================= #
+nwec = 4
+# Initial Design Vector
+r = 5
+wecx = [0, 0, 0, 0]
+wecy = [0, 10*r, 20*r, 30*r]
+d0 = 1e4
+damp = [d0, d0, d0, d0]
+x0 = np.zeros(1+3*nwec)
+x0[0] = r
+for i in range(nwec):
+    x0[1+i*3] = wecx[i]
+    x0[2+i*3] = wecy[i]
+    x0[3+i*3] = damp[i]
+bnds = [[2,8],   # radius
+        [0,0],[0,0],[0,1e7], # wec 1
+        [-100*r,100*r],[-100*r,100*r],[0,1e7],   # wec 2
+        [-100*r,100*r],[-100*r,100*r],[0,1e7],   # wec 3
+        [-100*r,100*r],[-100*r,100*r],[0,1e7]]   # wec 4
+# Parameters
+omega = 1.047
+A = 1.5
+rho_wec = 850
+p = [omega,A,rho_wec,nwec]
+opt={'xatol': 1e-3, 'disp': True}  
+# ================================================================================= #
+#                               Optimization Code                                   #
+# ================================================================================= #
+# Run Optimization
+best = A3.gradient_method(x0,p,bnds,opt)    #   Gradient Optimization
+# ================================================================================= #
+print(best)
--- nominal_design_nwec.py
+++ nominal_design_nwec.py
@@ -3,15 +3,16 @@ import numpy as np
 nwec = 4
 # Design Vector
 r = 5
-wecx = [0, 0, 0, 0]
-wecy = [0, 10*r, 20*r, 30*r]
-d = [1e5, 1e5, 1e5, 1e5, 1e5]
+wecy = [0, 0, 0, 0]
+wecx = [0, 10*r, 20*r, 30*r]
+d0 = 1e4
+damp = [d0, d0, d0, d0]
 x = np.zeros(1+3*nwec)
 x[0] = r
 for i in range(nwec):
     x[1+i*3] = wecx[i]
     x[2+i*3] = wecy[i]
-    x[3+i*3] = d[i]
+    x[3+i*3] = damp[i]
 # Parameters
 omega = 1.047
 A = 1.5
@@ -21,4 +22,3 @@ Power_out,efficiency,LCOE = model.run(x,p)
 print("Power out: ", Power_out)
 print("Efficiency: ", efficiency)
 print("LCOE: ", LCOE)
-
