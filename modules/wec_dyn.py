27a4194b67f41f616314a70d5f41d3ee956de3e5
Nate <118622380+degoeden@users.noreply.github.com>
2023-08-08 17:43:36 -0400

    negative dampings to match capytaine

--- modules/wec_dyn.py
+++ modules/wec_dyn.py
@@ -6,5 +6,5 @@ def wec_dyn(omega,F,A,B,C,m,d):
     k = (omega**2)*(m+A) - C    #   Calculate Optimal PTO stiffness
     if abs(k) > 1e7:            #   If the stiffness is above our max
         k = k/abs(k)*1e7        #   Cap it, but let it keep it's sign
-    XI = F/(-(A+m)*omega**2 + (B+d)*omega*1j + C+k)
+    XI = F/(-(A+m)*omega**2 - (B+d)*omega*1j + C+k)
     return XI,k
